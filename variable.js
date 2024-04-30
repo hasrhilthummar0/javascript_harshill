47bef246764a8fad230cdacde45ee3b3e3b4f7ce
harshil thummar <134331438+hasrhilthummar0@users.noreply.github.com>
2024-04-30 18:07:42 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,33 @@
+const Account_Number = 8963982;
+let Ammount = 1000;
+const Branch_name = 'yogichowk';
+let Account_Email = 'harshil89@gmail.com';
+var Mobile_Number = 982938290;
+
+// console.log(Account_Number,Ammount,Branch_name,Account_Email,Mobile_Number);
+
+
+/* when we chnage const varibale value is give a error ::
+assignment to constant variables
+
+Account_Number = 394928;
+console.log(Account_Number);
+
+*/
+
+
+Account_Email = 'harshilthummar9912@gmail.com'
+Ammount = 19000
+Mobile_Number = 99799809
+console.table([Account_Email , Ammount , Mobile_Number ])
+
+/*
+    when we use console.table it will give input in array and table sturcter
+    console.table([Account_Email , Ammount , Mobile_Number ])
+
+    in var is scope problem
+    in let is not scope problem
+
+    now we use let  keyword
+*/
+
