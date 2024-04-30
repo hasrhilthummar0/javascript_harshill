5c717dfd73a889e9b74203f472eb29d6d7475325
harshil thummar <134331438+hasrhilthummar0@users.noreply.github.com>
2024-04-30 18:28:04 +0530

    Add files via upload

---
+++ datatype_ecma.js
@@ -0,0 +1,11 @@
+     alert( 199 + 219)
+
+    let age =  19;
+    let name;
+    let surname = null;
+    console.log(typeof surname);
+    console.log(typeof name);
+    console.log(typeof age);
+
+
+    
